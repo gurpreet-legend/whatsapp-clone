7a0d80550e30bb193a151cb4d80319e08a2cb342
Gurpreet Singh <75157493+gurpreet-legend@users.noreply.github.com>
2021-12-12 19:45:56 +0530

    room functionality added

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import io from 'socket.io-client'
 import nanaid from "nanoid";
 import {useState, useEffect} from 'react'
 import { BiSend } from 'react-icons/bi';
+import { GoPlus } from "react-icons/go";
 
 //env stuff :
 // const PORT = process.env.PORT || 5000
@@ -10,18 +11,29 @@ const socket = io.connect('http://localhost:5000')
 
 function App() {
   const [message, setMessage] = useState('')
+  const [room, setRoom] = useState('')
+  const [online, setOnline] = useState(false);
   const [chat, setChat] = useState([])
 
     const sendChat = (e) => {
       e.preventDefault();
-      socket.emit("chat", {message})
+      const userID = socket.id;
+      if(room != '')
+        setChat([...chat, {message, userID, room}]);
+      socket.emit("chat", {message, userID, room})
       setMessage('')
+      // setRoom('');
+    }
+
+    const joinRoom = (e) => {
+      e.preventDefault();
+      socket.emit("join-room", room)
     }
 
     useEffect(() => {
       socket.on("recieve-message", (payload) => {
         setChat([...chat, payload])
-        console.log('uSEeFFECT CALLED')
+        // console.log('uSEeFFECT CALLED')
       })
     })
 
@@ -36,24 +48,41 @@ function App() {
         <img src='/whatsapp-bg.png' alt="bg-image" className='bgImg'/>
         {chat.map((payload, index) => {
           return (
-            <p key={index} className='chat-p'>{payload.message}</p>
+            <div className='chat-block'>
+              <p key={index}>{payload.message}</p>
+              <p className='userID'>{payload.userID}</p>
+            </div>
           )
         }
         )}
       </div>
-      <form onSubmit={sendChat} className='input-form'>
+      <div className="input-container">
+        <form onSubmit={sendChat} className='input-form'>
+          <input 
+            className='input-message'
+            type="text" 
+            name="chat" 
+            placeholder="Enter message" 
+            value={message} 
+            onChange={(e)=> {
+              setMessage(e.target.value);
+            }}/>
+            <button type="submit" className='btn'><BiSend/></button>
+        </form>
+        <form onSubmit={joinRoom} className='input-form'>
         <input 
-          className='input'
-          type="text" 
-          name="chat" 
-          placeholder="Enter message" 
-          value={message} 
-          onChange={(e)=> {
-            setMessage(e.target.value);
-          }}/>
-          <button type="submit" className='btn'><BiSend/></button>
-          
-      </form>
+              className='input'
+              type="text" 
+              name="room" 
+              placeholder="Enter room" 
+              value={room} 
+              onChange={(e)=> {
+                setRoom(e.target.value);
+              }}
+            />
+            <button type="submit" className='btn'><GoPlus/></button>
+        </form>
+      </div>
     </div>
   );
 }
--- server/server.js
+++ server/server.js
@@ -13,9 +13,20 @@ const PORT = process.env.PORT || 5000
 io.on('connection', (socket)=> {
     console.log('Socket is connected!!');
 
-    socket.on("chat", payload => {
+    socket.on("chat", (payload) => {
         console.log(payload)
-        io.emit("recieve-message", payload );
+        if(payload.room == ''){
+            const resObj = {message: "You dont have access to any room", userID: payload.userID, room: "", online: false};
+            socket.emit("recieve-message", resObj );
+        }
+        else{
+            const resObj = {message: payload.message, userID: payload.userID, room: payload.userID, online: true}
+            socket.broadcast.to(payload.room).emit("recieve-message", resObj);
+        }
+    })
+
+    socket.on("join-room", room => {
+        socket.join(room);
     })
     socket.on('diconnect', () => {
         console.log('User has screenLeft, socket has been disconnected !!');
