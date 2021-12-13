eed06b052fb05aca95a45c00c9d1b2e3a62e25db
Gurpreet Singh <75157493+gurpreet-legend@users.noreply.github.com>
2021-12-13 05:46:56 +0530

    firebase set-up

--- client/src/App.js
+++ client/src/App.js
@@ -5,38 +5,94 @@ import { BiSend } from 'react-icons/bi';
 import { GoPlus } from "react-icons/go";
 import OnlineUsers from './components/OnlineUsers';
 
+//FIREBASE SET-UP
+import { initializeApp } from 'firebase/app'
+import {
+    getFirestore, collection, onSnapshot,
+    addDoc, deleteDoc, doc,
+    query, where,
+    orderBy, serverTimestamp,
+    updateDoc
+} from 'firebase/firestore'
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB0XRbbB6kmMxp7tMBoolP0M1Jl_jEe9rg",
+  authDomain: "whatsapp-clone-a5402.firebaseapp.com",
+  projectId: "whatsapp-clone-a5402",
+  storageBucket: "whatsapp-clone-a5402.appspot.com",
+  messagingSenderId: "485282657816",
+  appId: "1:485282657816:web:81b123178ece90cfc3356d"
+};
+
+// init firebase
+initializeApp(firebaseConfig)
+
+// init services
+const db = getFirestore()
+
+
+
 //env stuff :
 // const PORT = process.env.PORT || 5000
 const socket = io.connect('http://localhost:5000')
 
 
 function App() {
+  
   const [message, setMessage] = useState('')
   const [room, setRoom] = useState('')
   const [onlineUsers, setOnlineUsers] = useState([]);
   const [chat, setChat] = useState([])
 
+  // collection ref
+  const roomsColRef = collection(db, 'rooms')
+  
+  // queries
+  const roomRef = query(roomsColRef, where("roomID", "==", room))
+  console.log(roomRef)
+  // const chatRef = doc(db,'rooms', room,  'chat')
+  // console.log(chatRef)
+
+
     const sendChat = (e) => {
       e.preventDefault();
       const userID = socket.id;
-      if(room != '')
+      if(room != ''){
         setChat([...chat, {message, userID, room}]);
+        // const roomRef = query(roomsColRef, where("roomID", "==", room))
+        // const chatRef = doc(db,'chat', )
+        // console.log(chatRef)
+        updateDoc(chatRef, {
+          message: message,
+          userID: userID,
+          userName: `${userID}+name`,
+          timeStamp: serverTimestamp()
+
+        })
+      }
 
       socket.emit("chat", {message, userID, room})
       setMessage('')
     }
 
     const getOnlineUsers = () => {
-      socket.emit("get-online-users", room)
-      socket.on("recieve-online-users", array => {
-        setOnlineUsers(array);
-      })
+      if(room != ""){
+        socket.emit("get-online-users", room)
+        socket.on("recieve-online-users", array => {
+            setOnlineUsers(array);
+        })
+      }
     }
 
     const joinRoom = (e) => {
       e.preventDefault();
       socket.emit("join-room", room)
+      localStorage.setItem("roomID", room)
       getOnlineUsers()
+      //////////////////////////////////////CHECK!!!!!!!
+      addDoc(roomsColRef, {
+        roomID: room
+      })
     }
 
 
@@ -46,6 +102,11 @@ function App() {
       })
     })
 
+    useEffect(() => {
+      let roomName = localStorage.getItem("roomID")
+      setRoom(roomName)
+      // getOnlineUsers()
+    }, [])
 
   return (
     <div className="App">
